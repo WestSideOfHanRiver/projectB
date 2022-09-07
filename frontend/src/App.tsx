2771d15450783b98683c01ec8414893d205a458f
bugeon <bearbu@naver.com>
2022-09-07 14:52:36 +0900

    chore: set typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,34 +27,35 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    state = {
-        posts: []
-    };
+	state = {
+		posts: [],
+	};
 
-    async componentDidMount() {
-        try {
-            const res = await fetch('http://127.0.0.1:8000/api/');
-            const posts = await res.json();
-            this.setState({
-                posts
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
+	async componentDidMount() {
+		try {
+			const res = await fetch('http://127.0.0.1:8000/api/');
+			const posts = await res.json();
+			this.setState({
+				posts,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-    render() {
-        return (
-            <div>
-                {this.state.posts.map(item => (
-                    <div key={item.id}>
-                        <h1>{item.title}</h1>
-                        <span>{item.content}</span>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				Hello TS
+				{/* {this.state.posts.map((item) => (
+					<div key={item.id}>
+						<h1>{item.title}</h1>
+						<span>{item.content}</span>
+					</div>
+				))} */}
+			</div>
+		);
+	}
 }
 
 export default App;
