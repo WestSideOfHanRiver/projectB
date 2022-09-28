8fb49c076d7b60f2300e97da7941747ab42f2d99
kbule771 <wansuk771@gmail.com>
2022-09-28 20:23:41 +0900

    [Feat] 모델 URL

--- projectb/main/urls.py
+++ projectb/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import ListArticle, DetailArticle
 
+
 urlpatterns = [
     path('', ListArticle.as_view()),
     path('<int:pk>/', DetailArticle.as_view()),
--- projectb/main/views.py
+++ projectb/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from rest_framework.response import Response
 from .models import Article
 from .serializers import AriticleSerializer
 
