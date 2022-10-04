8a0fe99dbc2c170cd3cbd3db5e28e556c49a6680
bu-geon <bearbu@naver.com>
2022-10-04 20:33:53 +0900

    chore: add axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import Main from 'routes/main';
 import GNB from 'routes/gnb';
 
 type selectType = 'shared' | 'views';
+const BASE_URL = 'http://127.0.0.1:8000/api/';
 
 const App = () => {
   const [select, setSelect] = useState<selectType>('shared');
   const [find, setFind] = useState('');
+
+  useEffect(() => {
+    const getApi = async () => {
+      const res = await axios.get(BASE_URL);
+    };
+    getApi();
+  }, []);
 
   return (
     <div className='App'>
@@ -24,65 +33,3 @@ const App = () => {
 };
 
 export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   // state = {
-//   //   posts: [],
-//   // };
-
-//   async componentDidMount() {
-//     try {
-//       const res = await fetch('http://127.0.0.1:8000/api/');
-//       const posts = await res.json();
-//       this.setState({
-//         // posts,
-//       });
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         Hello TS
-//         {/* {this.state.posts.map((item) => (
-// 					<div key={item.id}>
-// 						<h1>{item.title}</h1>
-// 						<span>{item.content}</span>
-// 					</div>
-// 				))} */}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
