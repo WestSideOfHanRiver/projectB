e62a3d00b8571eb10e5c178e699a1bd2cd85a8d9
kbule771 <wansuk771@gmail.com>
2022-12-05 21:00:17 +0900

    feat back post 통신완료

--- projectb/main/views.py
+++ projectb/main/views.py
@@ -1,37 +1,57 @@
-from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.response import Response
 from .models import Article
 from .serializers import AriticleSerializer
+from django.views.decorators.csrf import csrf_exempt
+# class ListArticle(generics.ListCreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = AriticleSerializer
 
 
-class ListArticle(generics.ListCreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = AriticleSerializer
-
-
-class DetailArticle(generics.ListCreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = AriticleSerializer
+# class DetailArticle(generics.ListCreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = AriticleSerializer
     
-    # 조회수 코드
-    def get(self, request,pk): 
-        queryset = Article.objects.get(id=pk)
-        queryset.hits += 1
-        queryset.save() 
-        serializer = AriticleSerializer(queryset)
-        return Response(serializer.data)
+#     # 조회수 코드
+#     def get(self, request,pk): 
+#         queryset = Article.objects.get(id=pk)
+#         queryset.hits += 1
+#         queryset.save() 
+#         serializer = AriticleSerializer(queryset)
+#         return Response(serializer.data)
     
  
-class ResultArticle(generics.ListCreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = AriticleSerializer
+# class ResultArticle(generics.ListCreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = AriticleSerializer
 
-    def get(request, score, pk): # json 파일
-        # queryset = Article.objects.get(id=pk)
-        # queryset.scores[pk] = score
-        # queryset.save()
-        return (HTTPResponse('넘어온 데이터'+score))
-
-    
+#     def get(request, score, pk): # json 파일
+#         # queryset = Article.objects.get(id=pk)
+#         # queryset.scores[pk] = score
+#         # queryset.save()
+#         return (HTTPResponse('넘어온 데이터'+score))
+@csrf_exempt
+def Result(request):
+    if request.method == 'GET':
+        # 페이지를 불러올 때
+        
+        print('GET')
+        return 
+    elif request.method == 'POST':
+        # 페이지 내에서 POST 발생 시
+        print('POST')
+        print(request)
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # re_password = request.POST['re-password']
+ 
+        # users = BookUsers(
+        #     username = username,
+        #     password = password
+        # )
+ 
+        # users.save()
+ 
+        return HttpResponse(request.method)
--- projectb/projectb/settings.py
+++ projectb/projectb/settings.py
@@ -57,7 +57,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
-                         ,'http://localhost:3000']
+                         ,'http://localhost:3000'
+                         ]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'projectb.urls'
